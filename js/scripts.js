ab988dee58916f23f488ae1bff46f9738a66a17e
Amber Tsosie <atxambra@gmail.com>
2018-11-14 14:02:42 -0800

    In-progress: installed handlebars and set up initial data object for project detials page.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var portfolio = {
+  projects: [
+    {
+      category: "Web Development",
+      title: "Personal Project",
+      subtitle: "for Research + Development",
+      mainLink: "github.com",
+      skills: ["Javascript", "React", "Redux", "Redux-Saga", "Git"],
+      description:
+        "Whitecaps lulu lemon seawall main street sedins main street lions rent translink rent rent developers seawall rain yoga gastown steam clock."
+    },
+    {
+      category: "Web Development + UI/UX Design",
+      title: "Final Student Project",
+      subtitle: "for Lighthouse Labs",
+      mainLink: "github.com",
+      skills: ["Ruby", "Rails", "ActiveRecord", "HTML", "CSS/SCSS" "Git"],
+      description:
+        "Cambie bridge chinatown rent rain new brunch spot on main street developers seawall ramen rent commercial stanley park gastown."
+    },
+  ]
+};
